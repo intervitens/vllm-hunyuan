caff9492fdcb7b87510c8f764a45bce12d5d61be
intervitens <intervitens@tutanota.com>
2024-11-06 00:34:07 +0300

    add HunYuanForCausalLM to model registry

--- vllm/model_executor/models/registry.py
+++ vllm/model_executor/models/registry.py
@@ -54,6 +54,7 @@ _TEXT_GENERATION_MODELS = {
     "JAISLMHeadModel": ("jais", "JAISLMHeadModel"),
     "JambaForCausalLM": ("jamba", "JambaForCausalLM"),
     "LlamaForCausalLM": ("llama", "LlamaForCausalLM"),
+    "HunYuanForCausalLM": ("hunyuan", "HunYuanForCausalLM"),
     # For decapoda-research/llama-*
     "LLaMAForCausalLM": ("llama", "LlamaForCausalLM"),
     "MambaForCausalLM": ("mamba", "MambaForCausalLM"),
